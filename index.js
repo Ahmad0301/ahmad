06e9214d583b9f1f14162ee4b97b37ea6a7385ef
Ahmad0301 <sehroz3040@gmail.com>
2024-10-26 13:29:29 +0500

    check url

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log(req.method,"url")
+    if (req.method === 'GET' && req.url=='/login')
+    {
+        console.log('login successfull')
+    }
+    else if (req.method === 'GET' && req.url=='/signup')
+        {
+            console.log('signup successfull')
+        }
 });
 const port =8000;
 server.listen(port,()=> {
